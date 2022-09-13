a81f859d8b509fde34da5a5a489969d21feab59e
inzapp <inzapp@naver.com>
2022-09-13 17:09:26 +0900

    rename file main.py -> cluster.py, change default training parameter

--- cluster.py
+++ cluster.py
@@ -2,11 +2,11 @@ from deep_cluster import DeepCluster
 
 if __name__ == '__main__':
     DeepCluster(
-        train_image_path=r'.',
+        train_image_path=r'/train_data/mnist',
         cluster_output_save_path=r'./clustered',
-        input_shape=(28, 28, 1),
+        input_shape=(32, 32, 1),
         encoding_dim=8,
-        lr=1e-3,
+        lr=0.001,
         epochs=20,
         batch_size=32,
         num_cluster_classes=10,
