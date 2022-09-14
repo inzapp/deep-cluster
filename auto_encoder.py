937d890a2bcd9b3f4bbdef3b409f1f9d1ce2de4d
inzapp <inzapp@naver.com>
2022-09-14 11:02:04 +0900

    update auto encoder model to convolutional

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,160 +1,47 @@
+import os
 import tensorflow as tf
+
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
 class AutoEncoder:
     def __init__(self, input_shape, encoding_dim):
-        self.scale_factor = 4
         self.input_shape = input_shape
         self.encoding_dim = encoding_dim
-        encoder_input, encoder_output, ae_output = self.__dense_ae()
+        self.latent_rows = self.input_shape[0] // 8
+        self.latent_cols = self.input_shape[1] // 8
+        self.latent_channels = 8192 // (self.latent_rows * self.latent_cols)
+        if self.latent_channels > 128:
+            self.latent_channels = 128
+        encoder_input, encoder_output, ae_output = self.__convolutional_ae()
         self.encoder = tf.keras.models.Model(encoder_input, encoder_output)
         self.ae = tf.keras.models.Model(encoder_input, ae_output)
+        self.ae.save('ae.h5', include_optimizer=False)
 
     def __convolutional_ae(self):
         encoder_input = tf.keras.layers.Input(shape=self.input_shape)
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(encoder_input)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=3,
-            strides=2,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            strides=2,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=128,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2D(
-            filters=128,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=1,
-            kernel_initializer='he_uniform',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
+        x = encoder_input
+        x = tf.keras.layers.Conv2D(strides=2, filters=32,  kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2D(strides=2, filters=64,  kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2D(strides=2, filters=128, kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2D(strides=1, filters=self.latent_channels, kernel_size=1, kernel_initializer='he_normal', padding='same', activation='relu')(x)
         x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.Dense(
-            units=self.encoding_dim,
-            activation='sigmoid')(x)
+        x = tf.keras.layers.Dense(units=2048, kernel_initializer='he_normal', activation='relu')(x)
+        x = tf.keras.layers.Dense(units=512, kernel_initializer='he_normal', activation='relu')(x)
+        x = tf.keras.layers.Dense(units=self.encoding_dim, kernel_initializer='glorot_normal', activation='linear')(x)
         encoder_output = x
 
-        scaled_feature_map_rows = int(self.input_shape[0] / self.scale_factor)
-        scaled_feature_map_cols = int(self.input_shape[1] / self.scale_factor)
-        x = tf.keras.layers.Dense(
-            units=32 * scaled_feature_map_rows * scaled_feature_map_cols,
-            kernel_initializer='he_uniform',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Reshape(target_shape=(scaled_feature_map_rows, scaled_feature_map_cols, 32))(x)
-        x = tf.keras.layers.Conv2DTranspose(
-            filters=128,
-            kernel_size=1,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2DTranspose(
-            filters=128,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2DTranspose(
-            filters=128,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2DTranspose(
-            filters=64,
-            kernel_size=3,
-            strides=2,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2D(
-            filters=64,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        x = tf.keras.layers.Conv2DTranspose(
-            filters=32,
-            kernel_size=3,
-            strides=2,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=3,
-            kernel_initializer='he_uniform',
-            padding='same',
-            use_bias=False)(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.ReLU()(x)
-
-        cae_output = tf.keras.layers.Conv2D(
-            filters=self.input_shape[2],
-            kernel_size=1,
-            activation='sigmoid')(x)
+        x = tf.keras.layers.Dense(units=512, kernel_initializer='he_normal', activation='relu')(x)
+        x = tf.keras.layers.Dense(units=2048, kernel_initializer='he_normal', activation='relu')(x)
+        x = tf.keras.layers.Dense(units=self.latent_rows * self.latent_cols * self.latent_channels, activation='relu')(x)
+        x = tf.keras.layers.Reshape(target_shape=(self.latent_rows, self.latent_cols, self.latent_channels))(x)
+        x = tf.keras.layers.Conv2DTranspose(strides=1, filters=128, kernel_size=1, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2DTranspose(strides=2, filters=128, kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2DTranspose(strides=2, filters=64,  kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2DTranspose(strides=2, filters=32,  kernel_size=3, kernel_initializer='he_normal', padding='same', activation='relu')(x)
+        x = tf.keras.layers.Conv2D(strides=1, filters=self.input_shape[-1], kernel_size=1, kernel_initializer='glorot_normal', padding='same', activation='sigmoid')(x)
+        cae_output = x
         return encoder_input, encoder_output, cae_output
 
     def __dense_ae(self):
@@ -162,16 +49,16 @@ class AutoEncoder:
         x = tf.keras.layers.Flatten()(encoder_input)
         x = tf.keras.layers.Dense(
             units=4096,
-            kernel_initializer='he_uniform',
+            kernel_initializer='he_normal',
             use_bias=False)(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
         encoder_output = tf.keras.layers.Dense(
             units=self.encoding_dim,
-            activation='sigmoid')(x)
+            activation='linear')(x)
         x = tf.keras.layers.Dense(
             units=4096,
-            kernel_initializer='he_uniform',
+            kernel_initializer='he_normal',
             use_bias=False)(encoder_output)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
