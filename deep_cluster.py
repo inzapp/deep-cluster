47a72d1d4a08f8a4f36de19d33ca81c68bb320c7
inzapp <inzapp@naver.com>
2024-01-30 14:04:06 +0900

    add license

--- deep_cluster.py
+++ deep_cluster.py
@@ -1,3 +1,25 @@
+"""
+Copyright (c) 2022 Inzapp
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 import os
 from concurrent.futures.thread import ThreadPoolExecutor
 from glob import glob
@@ -212,3 +234,4 @@ class DeepCluster(AutoEncoder):
             print(f'dir_path : {class_name:10s}, class index : {class_index:3d}, clustered data count : {clustered_data_count:6d}, real data count : {real_data_count:6d}, accuracy : {accuracy:.4f}')
         average_accuracy = acc_sum / float(num_classes)
         print(f'average accuracy : {average_accuracy:.4f}')
+
